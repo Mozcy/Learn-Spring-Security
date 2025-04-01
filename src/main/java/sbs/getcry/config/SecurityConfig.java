b390989fe1b08bfa4e16efcefb675329c2539176
Puppet <puppet.xiaoyi@gmail.com>
2025-04-01 15:39:59 +0800

    feat: 1.新增密码验证方式采用 BCryptPasswordEncoder 进行校验

---
+++ src/main/java/sbs/getcry/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package sbs.getcry.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // 直接返回 BCrypt 实例
+    }
+}
